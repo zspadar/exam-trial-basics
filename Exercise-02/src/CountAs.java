a0561c4986a1137f727e47cabc729b5c6950fbfa
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-04-10 11:01:45 +0200

    (add) almost working CountAsInFile function in Exercise-02

--- Exercise-02/src/CountAs.java
+++ Exercise-02/src/CountAs.java
@@ -1,19 +1,16 @@
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 
 /**
  * Created by zsuzsanna.padar on 2017. 04. 10..
  */
 public class CountAs {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Exception {
     final String FILE_NAME = "aFile.txt";
-    Path path = Paths.get(FILE_NAME);
-    try {
-      
+    countAsInFile(FILE_NAME);
 
-    } catch (Exception e){
-      System.out.println("No such file exsists - your result is 0");
-    }
     // Create a function that takes a filename as string parameter,
     // counts the occurances of the letter "a", and returns it as a number.
     // If the file does not exist, the function should return 0 and not break.
@@ -21,4 +18,24 @@ public class CountAs {
     // example: on the input "afile.txt" the function should return 28 - print this result
     // example: on the input "not-a-file" the function should return 0 - print this result
   }
+  public static void countAsInFile(String FILE_NAME) {
+    Path path = Paths.get(FILE_NAME);
+    try {
+      List<String> lines = Files.readAllLines(path);
+      String contentOfFile = String.join(".",lines);
+      int aCounter = 0;
+      char letterA = 'a';
+      while (aCounter < lines.size()) {
+        if (!(contentOfFile.charAt() = letterA)) {
+        } else {
+          letterA ++;
+        }
+        aCounter++;
+      }
+      System.out.println(aCounter);
+    } catch (Exception e){
+      System.out.println("No such file exists - your result is 0");
+    }
+  }
+
 }
