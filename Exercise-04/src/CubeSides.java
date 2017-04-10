657290947fc0db0d2577861bc0b1ab4f2a7cdedd
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-04-10 12:37:38 +0200

    (add) java class Cubesides to Exercise-04 and cretae constructor plus getters

---
+++ Exercise-04/src/CubeSides.java
@@ -0,0 +1,26 @@
+/**
+ * Created by zsuzsanna.padar on 2017. 04. 10..
+ */
+public class CubeSides {
+  private int a;
+  private int b;
+  private  int c;
+
+  public CubeSides(int a, int b, int c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+  }
+
+  public int getA() {
+    return a;
+  }
+
+  public int getB() {
+    return b;
+  }
+
+  public int getC() {
+    return c;
+  }
+}
